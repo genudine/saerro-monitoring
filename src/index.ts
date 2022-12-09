239385db4f274de849bee419d1c7a0cc39804032
Katalina Okano <git@kat.cafe>
2022-12-09 08:25:01 -0500

    new healthcheck api

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,14 @@ export interface Env {
   DISCORD_WEBHOOK_URL: string;
 }
 
-type WebsocketStatus = "PRIMARY" | "BACKUP" | "DOWN";
-type RedisStatus = "UP" | "DOWN";
+type UpDown = "UP" | "DOWN";
 
 type HealthResponse = {
   data: {
     health: {
-      redis: RedisStatus;
-      pc: WebsocketStatus;
-      ps4us: WebsocketStatus;
-      ps4eu: WebsocketStatus;
+      ingest: UpDown;
+      ingestReachable: UpDown;
+      database: UpDown;
     };
   };
 };
@@ -32,24 +30,23 @@ const sendAlert = (webhook: string, health?: HealthResponse) => {
 
   if (health) {
     const {
-      health: { redis, pc, ps4us, ps4eu },
+      health: { ingest, ingestReachable, database },
     } = health.data;
 
     embed.fields = [
-      { name: "Redis", value: redis },
-      { name: "PC", value: pc },
-      { name: "PS4 US", value: ps4us },
-      { name: "PS4 EU", value: ps4eu },
+      {
+        name: `Ingest Reachability`,
+        value: ingestReachable,
+      },
+      {
+        name: `Ingest`,
+        value: ingest,
+      },
+      {
+        name: `Database`,
+        value: database,
+      },
     ];
-
-    if (
-      pc !== "DOWN" &&
-      ps4us !== "DOWN" &&
-      ps4eu !== "DOWN" &&
-      redis !== "DOWN"
-    ) {
-      embed.color = 0xffd700;
-    }
   }
 
   return fetch(webhook, {
@@ -71,7 +68,7 @@ export default {
   ): Promise<void> {
     try {
       const res = await fetch(
-        "https://saerro.harasse.rs/graphql?query=%7B%20health%20%7B%20pc%20redis%20ps4us%20ps4eu%20%7D%7D"
+        "https://saerro.harasse.rs/graphql?query=%7B%20health%20%7B%20ingest%20ingestReachable%20database%7D%7D"
       );
 
       if (res.status !== 200) {
@@ -81,10 +78,9 @@ export default {
       const json: HealthResponse = await res.json();
 
       if (
-        json.data.health.redis !== "UP" ||
-        json.data.health.pc !== "PRIMARY" ||
-        json.data.health.ps4us !== "PRIMARY" ||
-        json.data.health.ps4eu !== "PRIMARY"
+        json.data.health.ingest !== "UP" ||
+        json.data.health.ingestReachable !== "UP" ||
+        json.data.health.database !== "UP"
       ) {
         console.error("Sending alert, failed checks", json);
         await sendAlert(env.DISCORD_WEBHOOK_URL, json);
